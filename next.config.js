4a59287b33ce39635d436c3e6393ac0ced066242
ridiculoid-net <ridiculoid.net@gmail.com>
2026-01-09 14:26:45 -0500

    update to next config and gitignore

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-    appDir: true
-  },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "i.imgur.com" },
+      { protocol: "https", hostname: "*.supabase.co" },
     ],
   },
 };
